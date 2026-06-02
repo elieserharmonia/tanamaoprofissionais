23d16e089bf3d233df45a14670b86ea11a7a94cb
elieserharmonia <eliesermusicoccb@gmail.com>
2026-06-02 17:11:25 -0300

    feat: create components index file for easier imports

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+// Layout Components
+export { Header } from './Header';
+export { BottomNavigation } from './BottomNavigation';
+
+// Navigation
+export { CategoryTabs } from './CategoryTabs';
+export { QuickAccessIcons } from './QuickAccessIcons';
+
+// Content Components
+export { ProfessionalCard } from './ProfessionalCard';
+export { ProfessionalGrid } from './ProfessionalGrid';
+export { PromoBanner } from './PromoBanner';
